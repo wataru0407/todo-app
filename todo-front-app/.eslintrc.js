22c8a98cc7a799dcf6b4ae65d9dc621bb51e2f8a
wataru0407 <wataru.tsukada47@gmail.com>
2022-12-25 17:22:50 +0900

    prettierとlintの設定を修正

--- todo-front-app/.eslintrc.js
+++ todo-front-app/.eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/typescript/recommended", "plugin:prettier/recommended"],
+  extends: ["eslint:recommended", "plugin:vue/vue3-recommended", "@vue/typescript", "prettier"],
   parserOptions: {
     ecmaVersion: 2020,
   },
@@ -11,5 +11,6 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "vue/multi-word-component-names": 0,
+    'vue/no-deprecated-slot-attribute': 'warn'
   },
 };
--- todo-front-app/src/router/index.ts
+++ todo-front-app/src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import ItemRegister from "../components/ItemRegister.vue";
 
 Vue.use(VueRouter);
 
@@ -16,7 +17,8 @@ const routes: Array<RouteConfig> = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   },
 ];
 
